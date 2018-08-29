1d0540cddb8b0fa79a68e7f911ef0908342a55c1
a1300 <matthias-knopp@gmx.net>
2018-08-30 01:50:16 +0200

    Add DI and logger

---
+++ DI.js
@@ -0,0 +1,43 @@
+const inversify = require('inversify')
+const helpers = require('inversify-vanillajs-helpers').helpers
+require('reflect-metadata')
+
+const logger = require('./logger.js')
+const init = require('./commands/init.js')
+const install = require('./commands/install.js')
+
+const DEPENDENCIES = {
+  LOGGER: 'Logger',
+  INIT: 'Init',
+  INSTALL: 'Install',
+}
+
+const container = new inversify.Container()
+
+// annotate
+helpers.annotate(init, [DEPENDENCIES.LOGGER])
+helpers.annotate(install, [DEPENDENCIES.LOGGER])
+
+const setup = () => {
+  // bindings
+  container.bind(DEPENDENCIES.INIT).to(int)
+  container.bind(DEPENDENCIES.INSTALL).to(install)
+
+  // constants or third party libraries
+  const registerConstantValue = helpers.registerConstantValue(container)
+  registerConstantValue(DEPENDENCIES.LOGGER, logger)
+}
+
+const resetConstants = () => {
+  container.unbindAll()
+  setup()
+}
+
+setup()
+
+module.exports = {
+  container,
+  DEPENDENCIES,
+  helpers,
+  resetConstants,
+}
---
+++ logger.js
@@ -0,0 +1,111 @@
+
+const { createLogger, format, transports } = require('winston')
+
+const {
+  combine,
+  timestamp,
+  printf,
+  label,
+} = format
+require('winston-daily-rotate-file')
+const moment = require('moment')
+const chalk = require('chalk')
+
+const logger = createLogger({
+  exitOnError: false,
+})
+
+const styleText = (text, meta) => {
+  if (meta) {
+    let result = eval(meta) // eslint-disable-line
+    text = result(text)
+  }
+  return text
+}
+
+// CONSOLE
+
+const customFormat = printf((info) => {
+  const formattedDate = moment(info.timestamp).format('YYYY-MM-DD HH:mm:ss.SSS')
+  let level = info.level.toUpperCase()
+  const chalkMessageBuild = ['chalk']
+  switch (level) {
+    case 'SILLY':
+      level = chalk.cyanBright(level)
+      chalkMessageBuild.push('cyanBright')
+      break
+    case 'DEBUG':
+      level = chalk.blueBright(level)
+      chalkMessageBuild.push('blueBright')
+      break
+    case 'VERBOSE':
+      level = chalk.magenta(level)
+      chalkMessageBuild.push('magenta')
+      break
+    case 'INFO':
+      level = chalk.cyan(level)
+      chalkMessageBuild.push('cyan')
+      break
+    case 'WARN':
+      level = chalk.yellow(level)
+      chalkMessageBuild.push('yellow')
+      break
+    case 'ERROR':
+      level = chalk.red(level)
+      chalkMessageBuild.push('red')
+      break
+    default:
+      break
+  }
+
+  if (info.meta && info.meta.length > 1) {
+    const separated = info.meta.split('.')
+    chalkMessageBuild.push(...separated)
+  }
+  const styles = chalkMessageBuild.join('.')
+
+  const message = styleText(info.message, styles)
+
+  // pass metadata to logger -> logger.info('test', { meta: 'meta' })
+  const output = `[${chalk.rgb(30, 144, 255)(formattedDate)}][${level}] ${message}`
+  return output
+})
+
+let consoleLogLevel = 'info'
+if (process.env.NODE_ENV === 'development') {
+  consoleLogLevel = 'verbose'
+}
+
+const con = new transports.Console({
+  level: consoleLogLevel,
+  format: combine(
+    label(),
+    timestamp(),
+    customFormat,
+  ),
+  colorize: process.stdout.isTTY,
+})
+logger.add(con)
+
+
+// FILE
+
+const fileFormatter = printf((info) => {
+  const msg = {
+    timestamp: moment().valueOf(),
+    level: info.level,
+    message: info.message,
+  }
+  return JSON.stringify(msg)
+})
+
+const dailyRotatefile = new (transports.DailyRotateFile)({
+  filename: 'asch-template-%DATE%.log',
+  dirname: 'logs',
+  datePattern: 'YYYY-MM-DD',
+  level: 'silly',
+  format: combine(fileFormatter),
+})
+logger.add(dailyRotatefile)
+
+module.exports = logger
